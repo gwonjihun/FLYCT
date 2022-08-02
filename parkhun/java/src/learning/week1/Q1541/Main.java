2dee5d10c0d75446dcb3929e645e1e2c176a3e2e
khPark <pkh543@naver.com>
2022-08-02 14:55:38 +0900

    second test

--- parkhun/java/src/learning/week1/Q1541/Main.java
+++ parkhun/java/src/learning/week1/Q1541/Main.java
@@ -1,6 +1,6 @@
-package java.week1;
+package learning.week1.Q1541;
 
-public class Q1541 {
+public class Main {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
