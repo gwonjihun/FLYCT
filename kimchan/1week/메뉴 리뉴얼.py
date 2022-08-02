746a044e0e8ea0b15b31c6a44c74966e55a8a361
Hitbee-dev <ckim.esw@gmail.com>
2022-08-02 22:57:35 +0900

    Solved: 메뉴 리뉴얼얼.py

--- kimchan/1week/메뉴 리뉴얼.py
+++ kimchan/1week/메뉴 리뉴얼.py
@@ -3,14 +3,18 @@ from itertools import *
 def solution(orders, course):
     result = {}
     for order in orders:
+        # combinations을 사용하기 위한 전제조건(입력으로 들어온 데이터의 정렬)
+        order = sorted(order)
+        order = "".join(order)
         for num in course:
-            for output in list(permutations(order, num)):
+            # 여기서 중요한 점은 permutations가 아닌 combinations를 사용하는 것이다.
+            # 아무리 브루트 포스 문제라고 해도, permutations는 중복 가능한 모든 값을 찾아내기 때문에 시간초과가 뜬다.
+            for output in list(combinations(order, num)):
                 data = "".join(list(output))
                 if data not in result:
                     result[data] = 1
                 else:
                     result[data] += 1
-
     answer = []
     for num in course:
         max_value = 0
