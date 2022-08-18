948ef11c02fe6dea140409b7233f8b3db2f8a52a
haebin <pgoqls@gmail.com>
2022-08-18 21:08:14 +0900

    Solved:8979.py

--- parkhaeb/2week/8979.py
+++ parkhaeb/2week/8979.py
@@ -1,7 +1,8 @@
 N, K = map(int, (input().split()))
 m = [list(map(int, (input().split()))) for _ in range(N)]
 
-s = sorted(m, key = lambda x : (-x[1], -x[2], -x[3])) # 익명함수 사용하여 메달 순서대로 정렬
+ # 익명함수 사용하여 메달(금, 은, 동) 순서대로 정렬
+s = sorted(m, key = lambda x : (-x[1], -x[2], -x[3]))
 
 # 등수 알고 싶은 국가의 인덱스 찾기
 for i in range(N) :
