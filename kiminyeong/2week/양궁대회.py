68c34737bed8d12859c8304088fb90247e200715
BBIYAC <kiy7833@gmail.com>
2022-08-19 00:40:58 +0900

    Solved: 양궁대회.py

--- kiminyeong/2week/양궁대회.py
+++ kiminyeong/2week/양궁대회.py
@@ -31,3 +31,43 @@ def solution(n, info):
             maxGap = l_score-a_score
             l_info = l_temp
     return l_info
+
+'''
+아래 코드는 중복 순열을 활용한 풀이 방식이다. -> 0~n까지의 숫자를 길이 11 리스트에 차례로 조합하여 계산
+정답이 도출되기는 하나, 시간이 매우 오래걸려 프로그래머스에서 시간초과가 발생한다.
+본 문제에서는 중복 조합을 활용하여 시간을 단축시킬 수 있다. -> 0~10까지의 인덱스를 n개씩 조합하여 계산
+
+from itertools import product
+
+def solution(n, info):
+    a_score = 0 # 어피치 점수
+    l_score = 0 # 라이언 점수
+    lion_arr = [] # 라이언 화살
+    # 중복 순열
+    for lions in product(range(n), repeat=11):
+        a_temp = 0 # 어피치 임시 점수
+        l_temp = 0 # 라이언 임시 점수
+        # n개의 화살 개수일 경우만 점수 계산
+        if sum(lions) != n:
+            continue
+        for i, lion in enumerate(lions):
+            # 라이언 화살수가 1보다 더 많은 경우는 건너뜀
+            if lion-info[i] > 1:
+                break
+            # 둘 다 0일땐 점수 X
+            if lion == 0 and info[i] == 0:
+                continue
+            # 라이언 화살 수가 1만큼 많으면 라이언 점수 획득
+            if lion - info[i] == 1:
+                l_temp += (10-i)
+            elif lion <= info[i]: # 어피치 화살 수가 더 많거나 같으면 어피치 점수 획득
+                a_temp += (10-i)
+        # 라이언 점수가 어피치 점수보다 크고, 라이언과 어피치의 점수차가 더 큰 경우 갱신
+        if l_temp > a_temp and l_temp-a_temp > l_score-a_score:
+            l_score, a_score = l_temp, a_temp
+            lion_arr = lions
+    # 라이언이 이길 수 없는 경우(어피치 점수가 크거나 같은 경우)
+    if a_score >= l_score:
+        lion_arr = [-1]
+    return list(lion_arr)
+'''
